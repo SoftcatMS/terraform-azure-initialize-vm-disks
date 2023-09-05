3f097bd64e7e73a2423e08d0c6b6621f5f1621b2
Nielsen Pierce <NielsenPi@softcat.com>
2023-09-05 13:54:39 +0100

    Add linux attachment sub module

---
+++ modules/linux/variables.tf
@@ -0,0 +1,11 @@
+## Linux
+
+variable "vm_password" {
+  description = "Password to connect to VM"
+  type        = string
+}
+
+variable "virtual_machine_id" {
+  description = "The virtual machine ID disks are attached to"
+  type        = string
+}
